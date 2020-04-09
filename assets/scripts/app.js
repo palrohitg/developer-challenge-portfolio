afc4f0d3dd80e879a12f40a7fc59d061bf1e0872
Vikas Pal <rohitprotobit@gmail.com>
2020-04-09 17:50:04 +0530

    Profile changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"I am a Machine Learning Enthusiast.", "love everything about code.", "also teach programming to people.", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
